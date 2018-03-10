a2ff779b5bdd075e7c96aebfcf1b06d82a0b5f7d
Joshua Needham <joshuabneedham@gmail.com>
2018-03-10 12:14:07 -0700

    Cleaning up User.rb per rubocop suggestions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,13 @@ class User < ApplicationRecord
     :rememberable,
     :trackable,
     :validatable,
-    :omniauthable,
-    omniauth_providers: [:facebook]
+    :omniauthable, omniauth_providers: [:facebook]
 
   validates :name, presence: true
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data == session['devise.facebook_data'] &&
-          session['devise.facebook_data']['extra']['raw_info']
+      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
         user.email = data['email'] if user.email.blank?
       end
     end
@@ -26,7 +24,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
     end
