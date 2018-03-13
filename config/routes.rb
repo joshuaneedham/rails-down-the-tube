6e1de89e079ea9f286fa5dd9585394b9d187ac50
Joshua Needham <joshuabneedham@gmail.com>
2018-03-13 09:03:52 -0600

    Updating routes and main layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resource :user do
-    resources :firearms, shallow: true
-  end
- 
+  resource :user
+  resources :firearms
+
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
