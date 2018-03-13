c74f9e8f257ddbec8813cbc3278290f7150cca67
Joshua Needham <joshuabneedham@gmail.com>
2018-03-13 08:57:19 -0600

    Cleaning up master branch

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -1,4 +1,3 @@
 class Outing < ApplicationRecord
-  belongs_to :user
-  belongs_to :firearm
+
 end
--- db/migrate/20180312230110_add_firearm_id_and_user_id_to_outings.rb
+++
@@ -1,6 +0,0 @@
-class AddFirearmIdAndUserIdToOutings < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :outings, :firearm, foreign_key: true
-    add_reference :outings, :user, foreign_key: true
-  end
-end
