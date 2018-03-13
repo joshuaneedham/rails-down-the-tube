5e72712dc8900af87fd71fa0ea51d738e60dd8d7
Joshua Needham <joshuabneedham@gmail.com>
2018-03-13 08:39:56 -0600

    Updated routes and removed nesting for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  resource :user do
-    resources :firearms, shallow: true
-  end
+  resources :firearms
   resources :barrels
   resources :outings
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'pages#index'
 end
